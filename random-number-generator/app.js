332ee394ff9f5afbe92206db154ba7476fc0a415
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-06-06 13:50:58 +0300

    Some tidying up to the app.js

--- random-number-generator/app.js
+++ random-number-generator/app.js
@@ -2,8 +2,10 @@ const myButton = document.getElementById("myButton");
 const label1 = document.getElementById("label1");
 const label2 = document.getElementById("label2");
 const label3 = document.getElementById("label3");
+
 const min = 1;
 const max = 6;
+
 let randomNum1;
 let randomNum2;
 let randomNum3;
