e0ed4f6992c172cce2977eccee06dc11b35357c3
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-07-17 17:04:34 +0300

    Roman numeral converter project done

--- roman-numeral-converter/script.js
+++ roman-numeral-converter/script.js
@@ -7,6 +7,25 @@ convertBtn.addEventListener("click", () => {
 })
 
 function convertToRomanian(number) {
+
+    const romanianCharacters = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    }
+
+    let roman = "";
+
     if (number === "") {
         output.style.display = "block";
         output.style.backgroundColor = "#be2929"
@@ -28,5 +47,13 @@ function convertToRomanian(number) {
         return;
     }
 
-    
+    for (let key in romanianCharacters) {
+        while (number >= romanianCharacters[key]) {
+            roman += key;
+            number -= romanianCharacters[key];
+        }
+    }
+
+    output.style.display = "block";
+    output.textContent = roman;
 }
